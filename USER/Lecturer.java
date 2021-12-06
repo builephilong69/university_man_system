5a78bb9f7ece1c16bc1087bd5da97ab972e0c9fb
builephilong69 <80462415+builephilong69@users.noreply.github.com>
2021-12-06 18:21:24 +0700

    Add files via upload

---
+++ USER/Lecturer.java
@@ -0,0 +1,20 @@
+package USER;
+
+public class Lecturer extends User {
+    public Lecturer(){};
+
+    private String faculty_id;
+    // private Course getCourseSummary(String course_id){}
+
+    @Override
+    public void getPersonalInfo() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public void printCourseSummary()
+    {
+        
+    }
+
+}

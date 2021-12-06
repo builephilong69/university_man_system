09f4b5cc5614b4273bbbf46fbcc20a9ef23f76fa
builephilong69 <80462415+builephilong69@users.noreply.github.com>
2021-12-06 18:28:50 +0700

    Create Student.java

---
+++ src/USER/Student.java
@@ -0,0 +1,21 @@
+package USER;
+import java.util.Date;
+
+public class Student extends User {
+
+    private String program_id;
+    private String intake;
+//  private Record record;
+    public Student() {
+    }
+
+    @Override
+    public void getPersonalInfo() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void viewRecord(){}
+    public void exportRecordToPDF(){}
+    
+}

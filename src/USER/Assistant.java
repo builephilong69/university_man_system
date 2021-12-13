9cace5567749dd96bb8f50d93f4cec977c71865c
Truc N.M. Bui <33834474+truc-bnm@users.noreply.github.com>
2021-12-13 21:42:17 +0700

    Update Assistant.java
    Adding: detailed methods' body

--- src/USER/Assistant.java
+++ src/USER/Assistant.java
@@ -5,15 +5,18 @@ package User;
 import java.util.Date;
 
 import GradeUtilities.Course;
+import GradeUtilities.Record;
+import sqlDBConnection.UserDBUtilities;
+import java.util.*;
 
-
-public class Assistant extends User {
+public class Assistant extends User{
     // data attributes
-
-    // contructors
+	ArrayList<Hashtable<String, String>> prog_falcuty;
+    // constructors
     public Assistant(String username) {
         super(username);
-
+        UserDBUtilities connector = new UserDBUtilities();
+        prog_falcuty = connector.getAssistantInfo(username);
     }
     // methods
     /***
@@ -22,60 +25,50 @@ public class Assistant extends User {
 	 */
     @Override
     public void getPersonalInfo() {
-        System.out.println("Program Assistant: "+this.info.getFullName());
-        this.info.getPersonalInfo();
-        
+        System.out.println("Program Assistant: " + this.info.getFullName());
     }
     /***
-	 * @param (student_id,course_id,Grade)
+	 * @param student_id, course_id, grade
 	 * @return none , insert the grade of a student
 	 */
-    public void insertGrade(String student_id,String course_id,float Grade){
-        
+    public void insertGrade(String student_id, String course_id, float grade){
+        UserDBUtilities connector = new UserDBUtilities();
+        connector.connect();
+        connector.insertGrade(student_id, course_id, grade);
+        connector.disconnect();
     }
     
     /***
-	 * @param (student_id,course_id,Grade)
+	 * @param student_id, course_id, grade)
 	 * @return none , modify the grade of a student
 	 */
-    public void modifyGrade(String student_id,String course_id,float Grade){
-
-    }
-    
-    
-    /***
-	 * @param (student_id)
-	 * @return A student's record
-	 */
-    private Record getStudentRecord(String student_id){
-        return null;
-        //to be added later
+    public void modifyGrade(String student_id, String course_id, float grade){
+    	UserDBUtilities connector = new UserDBUtilities();
+        connector.connect();
+        connector.modifyGrade(student_id, course_id, grade);
+        connector.disconnect();
     }
 
     /***
-	 * @param (record)
+	 * @param student_id
 	 * @return none , print a student record
 	 */
-    public void printStudentRecord(Record record){
-
+    public void printStudentRecord(String student_id){
+    	Record stu_record = new Record(student_id);
+    	stu_record.printRecord();
     }
 
-    /***
-	 * @param (course_id)
-	 * @return Course summary
-	 */
-    private Course getCourseSummary(String course_id){
-        return null;
-
-    } 
-
-    /***
-	 * @param (Course)
+     /***
+	 * @param course_id
 	 * @return none, print to the console Course Summary
 	 */
-    public void printCourseSummary(Course course){
-
-    }
-
-    
+    public void printCourseSummary(String course_id){
+    	Course course = new Course(course_id);
+    	System.out.println(course.getCourseInfo());
+    	System.out.println("Number of passing students: " + course.countNumPass());
+    	System.out.println("Number of failed students: " + course.countNumFailed());
+    	System.out.println("Highest grade: " + course.getMaxGrade());
+    	System.out.println("Lowest grade: " + course.getMinGrade());
+    	System.out.println("Class average: " + course.getAvr());
+    }   
 }
